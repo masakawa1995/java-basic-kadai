28c6f4dbef818f22ff6ea76d0345db98c33b1a60
Masaki_E <masakawa1995@gmail.com>
2025-04-12 22:31:23 +0900

    課題021：辞書プログラム追加

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,13 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+    public static void main(String[] args) {
+        Dictionary_Chapter21 dict = new Dictionary_Chapter21();
+
+        // 調べたい単語
+        String[] words = {"apple", "banana", "grape", "orange"};
+
+        dict.findMeaning(words);
+    }
+}
